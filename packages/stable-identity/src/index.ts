5d409c41b219af598a63f31541e2e5981c2d4876
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-22 17:45:08 +0800

    feat(node-04): expose stable identity API

---
+++ packages/stable-identity/src/index.ts
@@ -0,0 +1,7 @@
+export const STABLE_IDENTITY_ALGORITHM_VERSION = "1.0.0" as const;
+
+export * from "./hash.js";
+export * from "./identity.js";
+export * from "./mapping.js";
+export * from "./normalize.js";
+export type * from "./types.js";

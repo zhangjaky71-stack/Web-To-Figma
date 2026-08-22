5bc9a1b7f890042f1a810ccfc5e898a7202716c1
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-22 18:29:01 +0800

    feat(node-06): export source-provider API

---
+++ packages/source-providers/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./urls.js";
+export * from "./http-page-provider.js";
+export * from "./file-tab-provider.js";
+export * from "./local-folder-provider.js";
+export * from "./registry.js";

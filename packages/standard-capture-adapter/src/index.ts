43383c2da632c76089b98412255157b42f9638b3
zhangjaky71-stack <zhangjaky71@gmail.com>
2026-08-23 13:22:04 +0800

    feat(node-13): export Standard asset acquisition

--- packages/standard-capture-adapter/src/index.ts
+++ packages/standard-capture-adapter/src/index.ts
@@ -3,3 +3,4 @@ export * from "./privacy.js";
 export * from "./capture.js";
 export * from "./cascade-capture.js";
 export * from "./environment-capture.js";
+export * from "./asset-acquisition.js";
